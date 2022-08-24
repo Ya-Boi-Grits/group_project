19246b9f0ab2ca71ccbb13e4d53ea189beb79242
Jong Lee <76450939+Jojo970@users.noreply.github.com>
2022-08-23 21:29:52 -0500

    Delete server directory

--- server/routes/crypto.routes.js
+++
@@ -1,9 +0,0 @@
-const CryptoStrategyControllers = require("../controllers/crypto.controllers");
-
-module.exports = app => {
-    app.get('/api/cryptostrategy',CryptoStrategyControllers.findAllCryptoStrategys);
-    app.get('/api/cryptostrategy/:id',CryptoStrategyControllers.findOneCryptoStrategy);
-    app.post('/api/cryptostrategy',CryptoStrategyControllers.createCryptoStrategy);
-    app.put('/api/cryptostrategy/:id',CryptoStrategyControllers.updateCryptoStrategy);
-    app.delete('/api/cryptostrategy/:id',CryptoStrategyControllers.deleteCryptoStrategy);
-};
